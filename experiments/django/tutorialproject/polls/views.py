451e45d416e56487502f8ebf72c03d0bb43ed346
chaosarium <leonluleonlu@gmail.com>
2022-09-06 16:52:32 -0400

    done django tutorial part 3

--- experiments/django/tutorialproject/polls/views.py
+++ experiments/django/tutorialproject/polls/views.py
@@ -1,8 +1,37 @@
-# from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render
+import django as django
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django import http
 
 def index(request):
-    return HttpResponse("Hello, world. This is the poll index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    
+    return render(request, 'index.html', context)
+
+    # longer way
+    # template = loader.get_template('index.html')
+    # return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    question = django.shortcuts.get_object_or_404(Question, pk=question_id)
+    
+    # longer way
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise http.Http404("Question does not exist")
+    
+    return render(request, "detail.html", {'question': question})
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
