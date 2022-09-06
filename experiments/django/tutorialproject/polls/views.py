b072770725d7fc6ed57a43f2429c45783e23f222
chaosarium <leonluleonlu@gmail.com>
2022-09-06 19:07:08 -0400

    working answer voting system

--- experiments/django/tutorialproject/polls/views.py
+++ experiments/django/tutorialproject/polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 import django as django
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
 from django import http
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -34,4 +35,21 @@ def results(request, question_id):
     return HttpResponse(response)
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = django.shortcuts.get_object_or_404(Question, pk=question_id)
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else: 
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = django.shortcuts.get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
