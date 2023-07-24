78c3cf0a2d0dfe1f9ede7143549f5d9f4d901919
chaosarium <leonluleonlu@gmail.com>
2023-07-24 09:36:51 -0400

    better db result matching

--- influx_api/src/db/mod.rs
+++ influx_api/src/db/mod.rs
@@ -1,7 +1,9 @@
+#![allow(unused_imports)]
+
 use std::sync::Arc;
 use surrealdb::{
     Response, Surreal,
-    sql::Thing,
+    sql::{Thing, Object},
     engine::local::{File, Mem, Db},
 };
 // use surrealdb::{kvs::Datastore, dbs::Session, opt::auth::Root, engine::remote::ws::Ws, };
@@ -18,17 +20,15 @@ pub struct DB {
 impl DB {
     pub async fn create_db(disk: bool) -> Self {
         let client: Surreal<Db> = match disk {
-            true => Surreal::new::<File>("/Users/chaosarium/Desktop/temp.db").await.unwrap(),
+            true => Surreal::new::<File>("./temp.db").await.unwrap(),
             false => Surreal::new::<Mem>(()).await.unwrap()
         };
 
         client.use_ns("test").use_db("test").await.unwrap();
 
-        let db = DB {
+        DB {
             db: Arc::new(client),
-        };
-
-        db
+        }
     }
 
     pub async fn add_todo_sql(&self, text: String) -> Result<TodoInDB> { 
@@ -38,62 +38,40 @@ impl DB {
             .bind(("title", text))
             .await?;
 
-        println!("{:?}", &res);
-    
-        let idk: Option<TodoInDB> = res.take(0)?;
-        println!("{:?}", &idk);
-
-        Ok(idk.unwrap())
+        // dbg!(&res);
+        match res.take(0) {
+            Ok(Some::<TodoInDB>(v)) => Ok(v),
+            _ => Err(anyhow::anyhow!("Error creating todo"))
+        }
     }
 
     pub async fn get_todos_sql(&self) -> Result<Vec<TodoInDB>> {
         let sql = "SELECT * FROM todos";
-
         let mut res: Response = self.db
             .query(sql)
             .await?;
 
-        println!("{:?}", &res);
-        
-        let idk: Vec<TodoInDB> = res.take(0)?;
-        println!("{:?}", &idk);
-
-        Ok(idk)
+        // dbg!(res);
+        match res.take(0) {
+            Ok::<Vec<TodoInDB>, _>(v) => Ok(v),
+            _ => Err(anyhow::anyhow!("Error getting todos"))
+        }
     }
 
     pub async fn delete_todo_sql(&self, id: Thing) -> Result<()> {
         let sql = "DELETE todos:$id";
-
         let mut res: Response = self.db
             .query(sql)
             .bind(("id", id))
             .await?;
 
-        println!("{:?}", &res);
-        
-        let idk: Vec<TodoInDB> = res.take(0)?;
-        println!("{:?}", &idk);
-
-        Ok(())
+        // dbg!(res);
+        match res.take(0) {
+            Ok::<Vec<TodoInDB>, _>(v) => Ok(()),
+            _ => Err(anyhow::anyhow!("Error deleting todo"))
+        }
     }
 
-    // /// add a task to db and return it as object
-    // pub async fn add_task(&self, title: String) -> Result<(), crate::error::Error> {
-    //     println!("hi");
-    //     let created: Result<Task, _> = self.db.create("tasks")
-    //         .content(Task {
-    //             id: None,
-    //             title: title.clone(),
-    //             completed: false,
-    //             created_at: Some(Utc::now()),
-    //         })
-    //         .await;
-    //     println!("hi, we just created {:?}", created);
-
-    //     dbg!(&created);
-    //     Ok(())
-    // }
-    
 }
 
 
@@ -103,17 +81,33 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn db_test() {
+    async fn db_create() {
         let db = DB::create_db(false).await;
-        let add_todo_res = db.add_todo_sql("Hello world 1".into()).await.unwrap();
-        // let add_task_res = db.add_task_sql("Hello world 2".into()).await.unwrap();
-        // let add_task_res = db.add_task_sql("Hello world 3".into()).await.unwrap();
+        let todos = db.get_todos_sql().await.unwrap();
+        assert_eq!(todos.len(), 0);
+    }
 
-        println!("\n\nget tasks");
-        db.get_todos_sql().await.unwrap();
+    #[tokio::test]
+    async fn db_task_add() {
+        let db = DB::create_db(false).await;
+        let add_todo_res1 = db.add_todo_sql("Hello world 1".into()).await.unwrap();
+        assert_eq!(add_todo_res1.completed, false);
+        assert_eq!(add_todo_res1.text, "Hello world 1");
+        let add_todo_res2 = db.add_todo_sql("Hello world 2".into()).await.unwrap();
+        assert_eq!(add_todo_res2.completed, false);
+        assert_eq!(add_todo_res2.text, "Hello world 2");
+        let add_todo_res3 = db.add_todo_sql("Hello world 3".into()).await.unwrap();
+        assert_eq!(add_todo_res3.completed, false);
+        assert_eq!(add_todo_res3.text, "Hello world 3");
 
-        // let add_task_res = db.add_task("Hello world".into()).await.unwrap();
+        assert_ne!(add_todo_res1.id, add_todo_res2.id);
+        assert_ne!(add_todo_res1.id, add_todo_res3.id);
 
+        let todos = db.get_todos_sql().await.unwrap();
+        assert_eq!(todos.len(), 3);
+        for todo in todos {
+            println!("{:?}", todo);
+        }
     }
 
 }
--- influx_api/src/db/models.rs
+++ influx_api/src/db/models.rs
@@ -2,17 +2,21 @@
 
 use serde::{Deserialize, Serialize};
 use surrealdb::sql::{Thing, Array, Object, Value};
+use ts_rs::TS;
 
 use crate::map;
 // use crate::utils::macros::map;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, TS)]
+#[ts(export, export_to = "../bindings/")]
 pub struct TodoInDB {
     #[serde(skip_serializing_if = "Option::is_none")]
+    #[ts(type = "string")]
     pub id: Option<Thing>,
     pub text: String,
     pub completed: bool,
 }
+
 impl From<TodoInDB> for Value {
     fn from(val: TodoInDB) -> Self {
         match val.id {
@@ -28,5 +32,23 @@ impl From<TodoInDB> for Value {
             ]
             .into(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_todo_in_db() {
+        let todo = TodoInDB {
+            id: None,
+            text: "text".into(),
+            completed: false,
+        };
+
+        let val: Value = todo.into();
+
+        println!("{:?}", val);
     }
 }
