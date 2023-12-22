40c9a0bdca4a0010b635b19febd092ca41bdb5b2
chaosarium <leonluleonlu@gmail.com>
2023-12-22 16:55:12 -0500

    display ditail for last clicked / last hovered token

--- influx_ui/src/lib/components/Token.svelte
+++ influx_ui/src/lib/components/Token.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
   import Tooltip from "./TokenTooltip.svelte";
+  import { createEventDispatcher } from 'svelte';
+
   export let token;
+
+  const dispatch = createEventDispatcher();
+  const handleMouseEnter = () => {
+    dispatch('token_hover', token.orthography);
+  };
+  const handleClick = () => {
+    dispatch('token_click', token.orthography);
+  };
+
+
 </script>
 
 <Tooltip token={token}>
@@ -13,6 +25,8 @@
     class:border-amber-400={token.status === 'L3'}
     class:border-lime-400={token.status === 'L4'}
     class:border-teal-400={token.status === 'L5'}
+    on:mouseenter={handleMouseEnter}
+    on:click={handleClick}
   >{token.orthography}</span><span class="border-solid bg-blue-50 px-1"> </span>
   
 </Tooltip>
--- influx_ui/src/routes/texts/[lang]/[file]/+page.svelte
+++ influx_ui/src/routes/texts/[lang]/[file]/+page.svelte
@@ -4,7 +4,16 @@
   import DbgGlobalNav from "$lib/dbg/DbgGlobalNav.svelte";
   import DbgJsonData from "$lib/dbg/DbgJsonData.svelte";
   import Token from "$lib/components/Token.svelte";
-  import Tooltip from "$lib/components/TokenTooltip.svelte";
+  
+  let lastHoveredOrth = '';
+  let lastClickedOrth = '';
+  const handleHover = event => {
+    lastHoveredOrth = event.detail;
+  };
+  const handleClick = event => {
+    lastClickedOrth = event.detail;
+  };
+
 </script>
 <DbgGlobalNav />
 
@@ -13,13 +22,42 @@ Page
 <p>File `{$page.params.file}`</p>
 <hr>
 
+<p>Title:</p>
+
 <h1 class="font-bold">{data.metadata.title}</h1>
 
-<div class="p-4">
+<p>Text area:</p>
+
+<div class="p-4 leading-10">
   {#each data.tokens as token}
-    <Token {token} />
+    <Token {token} 
+      on:token_hover={handleHover} 
+      on:token_click={handleClick}
+    />
   {/each}
 </div>
+
+<div class="p-4 bg-rose-50">
+  <p>Last hovered token:</p>
+  <p><b>{lastHoveredOrth}</b></p>
+  <ol>
+    <li>lemma:</li>
+    <li>language:</li>
+    <li>status:</li>
+    <li>phonetic:</li>
+  </ol>
+</div>
+<div class="p-4 bg-amber-50">
+  <p>Last hovered token:</p>
+  <p><b>{lastClickedOrth}</b></p>
+  <ol>
+    <li>lemma:</li>
+    <li>language:</li>
+    <li>status:</li>
+    <li>phonetic:</li>
+  </ol>
+</div>
+
 
 
 
