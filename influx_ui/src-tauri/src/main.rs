48a1c1d1adc32c76226046e58562e39a9e445c30
chaosarium <leonluleonlu@gmail.com>
2024-01-11 19:33:59 -0500

    tauri spawns a influx_server thread

--- influx_ui/src-tauri/src/main.rs
+++ influx_ui/src-tauri/src/main.rs
@@ -1,8 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[allow(unused_imports)]
 
-fn main() {
+use std::thread;
+
+#[tokio::main]
+async fn main() {
+
+  tokio::spawn(async move {
+    println!("spawning influx_server");
+    let _launched = influx_server::launch(false, true, "/Users/chaosarium/Documents/Dev/Influx/toy_content".into()).await;
+  });
+
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-#[tokio::main]
-#[deprecated]
-async fn main() {
-    panic!("This is deprecated for now. Use influx_api::main() instead.");
-}
