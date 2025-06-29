1d664cb93679cf0e4dcbd1e9a30b955756747ea2
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2025-06-29 16:22:49 -0400

    whatever

--- influx_client/src/Utils/FancyMouse.elm
+++ influx_client/src/Utils/FancyMouse.elm
@@ -4,21 +4,13 @@ import Html
 import Html.Events.Extra.Mouse as Mouse
 
 
+onAltMouseUp : msg -> msg -> Html.Attribute msg
+onAltMouseUp msg noop_msg =
+    Mouse.onUp
+        (\event ->
+            if event.keys.alt then
+                msg
 
--- onAltMouseUp : msg -> Html.Attribute msg
--- onAltMouseUp msg =
---     Mouse.onUp
---         (\event ->
---             if event.altKey then
---                 msg
---             else
---                 Mouse.NoOp
---         )
--- e.g.
--- Mouse.onDown
---     (\event ->
---         if event.keys.alt then
---             args.mouse_handler (FocusContext.SelectMouseDown cst)
---         else
---             Debug.todo "ah"
---     )
+            else
+                noop_msg
+        )
