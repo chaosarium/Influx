5aba91c57d5c700a5bfc7d999134ade91d0a3fe8
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2025-07-12 09:42:08 -0400

    instructions

---
+++ influx_nlp/lib/annotation.py
@@ -0,0 +1,2 @@
+# TODO define types and classes that correspond to AnnotatedDocV2 in `influx_core/src/lib.rs`. 
+# The class should expose .to_dict() that returns the representation which can then be serialized to JSON and decoded in Rust.
